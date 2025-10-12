e5bfeb8c903edb7f8081859f9926597d5cbcd778
Tien Dat Pham <dat20036@gmail.com>
2025-10-12 19:16:20 +0700

    another take on tackling ESLint.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    typedRoutes: true,
     compiler: {
         removeConsole: process.env.NODE_ENV === "production",
     },
+    typedRoutes: true,
 };
 
 export default nextConfig;
