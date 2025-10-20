e6301d42c4a16c940feb64c60ce60198ea0a176a
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-10-20 09:36:47 +0700

    chore: remove trash

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -62,17 +62,17 @@ export default function Timeline({ selectedType }: { selectedType: keyof Project
     return (
         <div>
             <div className={"relative max-w-[900px] w-full mx-auto my-[60px] px-] py-[20px] flex justify-center items-center"}>
-                <div className={"relative w-[1000px] h-[4px] bg-[linear-gradient(to_right,_lightgray,_darkgray,_gray,_black,_gray,_darkgray,_lightgray)] rounded-[4px]"}>
+                <div className={"relative w-[1000px] h-[4px] bg-[linear-gradient(to right, lightgray, darkgray, gray, black, gray, darkgray, lightgray)] rounded-[4px]"}>
                     {visibleItems.map((item, index_) => {
                         const leftPercent = (index_ + ((visibleItems.length - 1) / 2 - index_) * 0.1) / (visibleItems.length - 1) * 100;
                         return (
                             <div
-                                className={"absolute top-2/4 -translate-y-1/2 w-[18px] h-[18px] bg-[#fff] border-[3px] border-[#000] rounded-[50%] cursor-pointer [transition:transform_0.2s_ease]"}
+                                className={"absolute top-2/4 -translate-y-1/2 w-[18px] h-[18px] bg-[#fff] border-[3px] border-[#000] rounded-[50%] cursor-pointer"}
                                 key={item.name}
                                 style={{ left: `${leftPercent}%` }}
                             >
                                 <div className={"absolute top-[28px] left-2/4 -translate-x-1/2 text-[14px] font-semibold whitespace-nowrap"}>{item.date}</div>
-                                <div className={"relative top-[50px] left-2/4 -translate-x-1/2 min-w-[180px] min-h-[200px] p-[10px] bg-[var(--background)] border-[1px] border-[#ccc] rounded-[8px] text-center [box-shadow:0_0_8px_rgba(0,_0,_0,_0.15)] opacity-100 block [transition:opacity_0.2s_ease-in-out] z-10"}>
+                                <div className={"relative top-[50px] left-2/4 -translate-x-1/2 min-w-[180px] min-h-[200px] p-[10px] bg-[var(--background)] border-[1px] border-[#ccc] rounded-[8px] text-center [box-shadow:0_0_8px_rgba(0, 0, 0, 0.15)] opacity-100 block z-10"}>
                                     <div className={"text-2xl mb-5"}>{item.name}</div>
                                     <div className={"text-base"}>{item.content}</div>
                                 </div>
