2720de1c48230c87fc8f716a4cb64709ae544a04
Tien Dat Pham <dat20036@gmail.com>
2025-10-21 10:07:48 +0700

    add donation button. (#12)
    we have donation before ARC Raiders release.

--- src/components/FatFooter.tsx
+++ src/components/FatFooter.tsx
@@ -1,11 +1,21 @@
 import FacebookLogo from "@public/brand/facebook.svg";
 import SteamLogo from "@public/brand/steam.svg";
 import YoutubeLogo from "@public/brand/youtube.svg";
+import VNS_Donate from "@public/VNS_Donate.jpg";
 import VNS_Logo from "@public/VNS_Logo.png";
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, HeartHandshake } from "lucide-react";
 import { Route } from "next";
 import Image from "next/image";
 import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent, DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 
 const navigations: {
     group: string;
@@ -52,7 +62,7 @@ const navigations: {
  */
 export default function FatFooter() {
     return (
-        <footer className={"flex min-h-72 w-full flex-col justify-between bg-neutral-200 shadow-sm shadow-primary dark:bg-background"}>
+        <footer className={"flex min-h-72 gap-2 flex-col justify-between bg-neutral-200 shadow-sm shadow-primary dark:bg-background"}>
             <div className={"flex flex-col justify-evenly gap-y-6 md:flex-1/2 md:flex-row md:place-items-center-safe"}>
                 {/* Logo */}
                 <div className={"mt-8 flex w-full flex-row place-items-center justify-evenly gap-4 md:w-fit md:flex-col [&_img]:dark:invert"}>
@@ -94,10 +104,34 @@ export default function FatFooter() {
                     </section>
                 ))}
             </div>
-            <div className={"my-4 text-center font-bold italic"}>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button className={"max-w-sm self-center font-bold"} variant={"destructive"}>
+                        <HeartHandshake className={""} />
+                        Donate cho Arknights VNS
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle className={"text-2xl font-bold"}>❤️ Donate</DialogTitle>
+                        <DialogDescription>
+                            Cảm ơn bạn đã ủng hộ Arknights VNS!
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className={"flex flex-col space-y-2"}>
+                        <p>
+                            Bằng việc Donate, các bạn đã góp phần giúp tụi mình trang trải chi phí thuê địa điểm Offline,
+                            {" "}
+                            đặt commission, mua merch Arknights, cũng như bảo trì hạ tầng IT.
+                        </p>
+                        <Image alt={"amiya_placeholder"} className={"self-center"} src={VNS_Donate} width={320} />
+                    </div>
+                </DialogContent>
+            </Dialog>
+            <div className={"my-4 text-center font-bold italic text-sm"}>
                 ©
                 {" "}
-                <Link className={"font-extrabold text-red-600"} href={"/"}>
+                <Link className={"font-extrabold text-red-600 dark:text-muted-foreground"} href={"/"}>
                     Arknights Vietnam Station
                 </Link>
                 , 2020-nay.
