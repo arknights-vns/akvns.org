b2d533685120cfa2625889004ead3cd564f1f0db
James-cmd-147 <s4112913@rmit.edu.vn>
2025-10-23 23:39:39 +0700

    misc: added responsiveness

--- src/app/(vns)/FAQ/FaqList.tsx
+++ src/app/(vns)/FAQ/FaqList.tsx
@@ -33,29 +33,30 @@ export default function FAQAccordion() {
   const [openId, setOpenId] = useState<number | null>(null);
 
   return (
-    <div className="max-w-2xl mx-auto py-8">
-      <h2 className="text-3xl font-bold text-center mb-6 text-[#FE0606]">Câu hỏi thường gặp</h2>
+    <div className="max-w-2xl mx-auto py-6 sm:py-4 px-3 sm:px-4 md:px-6">
+      <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6 text-[#FE0606]">Câu hỏi thường gặp</h2>
 
       {faqs.map((faq, id) => (
 
         <div
+        // using derive stable key to avoid noArrayIndexKey
           key={faq.question}
           className={`mb-4 rounded-xl shadow-lg/20 overflow-hidden bg-gray-200`}
         >
         {/* Question container */}
           <button
           type="button"
-            className="w-full flex justify-between items-center px-6 py-4 text-left font-semibold text-lg"
+            className="w-full flex justify-between items-center px-6 py-4 text-left font-semibold sm:px-6 sm:py-3 sm:text-lg lg:text-xl"
             onClick={() => setOpenId(openId === id ? null : id)}
           >
              <span>{`Q${id + 1}. ${faq.question}`}</span> {/* increase question number without string concatenation */}
-            <span className="text-2xl text-[#FE0606]">
+            <span className="text-xl sm:text-2xl text-[#FE0606]">
               {openId === id ? "X" : "+"}
             </span>
           </button>
         {/* Answer container */}
           {openId === id && (
-            <div className="px-6 pb-4 pt-2 bg-gray-50 text-gray-800">{faq.answer}</div>
+            <div className="px-6 pb-4 pt-2 bg-gray-50 text-gray-800 text-sm sm:text-base ">{faq.answer}</div>
           )}
         </div>
       ))}
