8677037f44ac11eefe252aa3513ac78db09cf9c8
James-cmd-147 <s4112913@rmit.edu.vn>
2025-10-24 00:10:51 +0700

    misc: move data to a seperate json file and add id for each question

--- src/app/(vns)/FAQ/FaqList.tsx
+++ src/app/(vns)/FAQ/FaqList.tsx
@@ -1,33 +1,7 @@
 "use client"
 
 import { useState } from "react";
-
-const faqs = [
-  {
-    question: "Tus có đa thê không?",
-    answer: (
-      <ul className="list-disc ml-6">
-        <li>Đụt: Có, cực kỳ nhiều.</li>
-        <li>
-          Kamito, Meruko, Sadus, giabao06, shiina_kochiya, Shihotori, Succhusa,
-          jameshanderson, nonamefornow, Acsyncct, Peashooter: +1
-        </li>
-      </ul>
-    ),
-  },
-  {
-    question: "Tus có bao nhiêu con vợ?",
-    answer: "Too many, can't be counted.",
-  },
-  {
-    question: "Tus có bao nhiêu con vợ?",
-    answer: "Too many, can't be counted.",
-  },
-  {
-    question: "Tus có bao nhiêu con vợ?",
-    answer: "Too many, can't be counted.",
-  },
-];
+import faqsData from "@/app/(vns)/data/faqsData.json"
 
 export default function FAQAccordion() {
   const [openId, setOpenId] = useState<number | null>(null);
@@ -36,11 +10,10 @@ export default function FAQAccordion() {
     <div className="max-w-2xl mx-auto py-6 sm:py-4 px-3 sm:px-4 md:px-6">
       <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6 text-[#FE0606]">Câu hỏi thường gặp</h2>
 
-      {faqs.map((faq, id) => (
+      {faqsData.map((faq, id) => (
 
         <div
-        // using derive stable key to avoid noArrayIndexKey
-          key={faq.question}
+          key={faq.id}
           className={`mb-4 rounded-xl shadow-lg/20 overflow-hidden bg-gray-200`}
         >
         {/* Question container */}
@@ -56,7 +29,13 @@ export default function FAQAccordion() {
           </button>
         {/* Answer container */}
           {openId === id && (
-            <div className="px-6 pb-4 pt-2 bg-gray-50 text-gray-800 text-sm sm:text-base ">{faq.answer}</div>
+            <div className="px-6 pb-4 pt-2 bg-gray-50 text-gray-800 text-sm sm:text-base">
+              <ul className="list-disc ml-6">
+                {faq.answer.map((line, _id) => (
+                  <li  key={`${faq.id}-${_id}`}>{line}</li> 
+                ))}
+              </ul>
+            </div>
           )}
         </div>
       ))}
