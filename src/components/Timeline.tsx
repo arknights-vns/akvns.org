88f0563f057693d20bb50e5c93cfa89878e27314
Kochiya Shiina <caovanviethung1412@gmail.com>
2025-10-28 12:45:15 +0700

    chore: bye bye hard code sizes

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -38,18 +38,18 @@ export default function Timeline({ selectedType }: { selectedType: keyof Project
 
     return (
         <div>
-            <div className={"relative max-w-[900px] w-full mx-auto my-[60px] px-0 py-[20px] flex justify-center items-center"}>
-                <div className={"relative w-[1000px] h-1 bg-linear-to-r from-gray-600 via-gray-400 to-gray-600 rounded-[4px]"}>
+            <div className={"relative max-w-5xl w-full mx-auto my-15 px-0 py-5 flex justify-center items-center"}>
+                <div className={"relative w-5xl h-1 bg-linear-to-r from-gray-600 via-gray-400 to-gray-600 rounded-[4px]"}>
                     {visibleItems.map((item, index_) => {
                         const leftPercent = (index_ + ((visibleItems.length - 1) / 2 - index_) * 0.1) / (visibleItems.length - 1) * 100;
                         return (
                             <div
-                                className={"absolute top-2/4 -translate-y-1/2 w-[18px] h-[18px] bg-[#ffffff] border-[3px] border-[#000000] rounded-[50%] cursor-pointer"}
+                                className={"absolute top-2/4 -translate-y-1/2 w-5 h-5 bg-[#ffffff] border-3 border-[#000000] rounded-[50%] cursor-pointer"}
                                 key={item.name}
                                 style={{ left: `${leftPercent}%` }}
                             >
-                                <div className={"absolute top-[28px] left-2/4 -translate-x-1/2 text-[14px] font-semibold whitespace-nowrap"}>{item.date}</div>
-                                <div className={"relative top-[50px] left-1/2 -translate-x-1/2 min-w-[180px] min-h-[200px] p-3 bg-background border border-[#cccccc] rounded-[8px] text-center [box-shadow:0_0_8px_rgba(0, 0, 0, 0.15)] block z-10"}>
+                                <div className={"absolute top-7 left-2/4 -translate-x-1/2 text-4 font-semibold whitespace-nowrap"}>{item.date}</div>
+                                <div className={"relative top-13 left-1/2 -translate-x-1/2 min-w-45 min-h-50 p-3 bg-background border border-[#cccccc] rounded-[8px] text-center [box-shadow:0_0_8px_rgba(0, 0, 0, 0.15)] block z-10"}>
                                     <div className={"text-2xl mb-5"}>{item.name}</div>
                                     <div className={"text-base"}>{item.content}</div>
                                 </div>
