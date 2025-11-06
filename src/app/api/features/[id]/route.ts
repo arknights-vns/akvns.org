618beb2588f51eedc6b0532c5f976def2c7bfe9a
Tien Dat Pham <dat20036@gmail.com>
2025-11-06 09:25:04 +0700

    build Prisma for MUSL also.
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/app/api/features/[id]/route.ts
+++ src/app/api/features/[id]/route.ts
@@ -2,7 +2,7 @@ import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 import { auth } from "@/lib/auth";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { FeatureFlag } from "@/schema/feature";
 
 /**
