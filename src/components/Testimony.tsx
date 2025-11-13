c60072942a1020659794bbe468daf9ad5260131e
DOTANLOC <itcsiu21199@student.hcmiu.edu.vn>
2025-11-13 20:46:23 +0700

    2nd worst testimony

--- src/components/Testimony.tsx
+++ src/components/Testimony.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import React from "react";
 
-import "@/app/globals.css";
 import {
     Card,
     CardContent,
@@ -12,73 +11,97 @@ import {
 type CardData = {
     avatar: string;
     description: string;
+    id: string;
     info: string;
     name: string;
+
 };
 
 const reviews: CardData[] = [
+
     {
-        avatar: "/members/meruko.png",
-        description: "Jennie, chính thất, aka, Vợ cả.",
-        info: "@jennienguyn",
-        name: "Nguyễn Thị Linh Chi",
+
+        avatar: "/amiya.png",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.",
+        id: "review-1",
+        info: "@an_hai",
+        name: "BLYAT",
     },
     {
-        avatar: "/members/meruko.png",
-        description: "1 con mèo khác, nhưng thua vợ cả.",
-        info: "@yuki_chino",
-        name: "Chino",
+
+        avatar: "/amiya.png",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.",
+        id: "review-2",
+        info: "@Eggs",
+        name: "Dut za Liems",
     },
     {
-        avatar: "/members/meruko.png",
-        description: "Người Việt N1 tiếng Nhật chửi tụi CS bằng tiếng Nga.",
-        info: "@hitorimi_achi",
-        name: "Hitorimi Achi",
+        avatar: "/amiya.png",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.",
+        id: "review-3",
+        info: "@Dut",
+        name: "Dut!!",
     },
     {
-        avatar: "/members/meruko.png",
-        description: "Chị gái hàng xóm hát hay, ghét tôm.",
-        info: "@renjiwatarimono",
-        name: "Tee",
+        avatar: "/amiya.png",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.",
+        id: "review-4",
+        info: "@JohnBalatro",
+        name: "nan-inf từ ante 0",
     },
+
 ];
 
 export default function Testimony() {
     return (
+
         <div className={"max-w-4xl mx-auto px-6"}>
             <div className={"text-center mb-10"}>
-                <div className={"text-2xl font-bold text-primary mb-2"}>Oguro có bao nhiêu người yêu?</div>
-                <p className={"text-accent-foreground"}>Sau khi bị @Swyrin VAR, Oguro có:</p>
+                <h2 className={"text-3xl font-bold text-[#F25C5C] mb-2"}>
+                    Mọi người nghĩ gì về mình?
+                </h2>
+                <p className={"text-accent-foreground text-xl"}>
+                    Overwhelming Negative Reviews:
+                </p>
             </div>
 
-            <section className={"grid grid-cols-1 md:grid-cols-2 gap-8"}>
+            <section className={"grid grid-cols-1 sm:grid-cols-2 gap-8"}>
                 {reviews.map(c => (
                     <Card
-                        className={"border z shadow-xl "}
-                        key={c.info}
+                        className={"border border-accent-foreground shadow-[0_0_20px_4px_rgba(255,0,0,0.2)]"}
+                        key={c.id}
                     >
-                        <CardHeader className={"flex flex-col items-center text-center py-6"}>
-                            <div className={"flex flex-cl items-center gap-3"}>
-                                <div className={"rounded-full overflow-hidden border-1 shadow-md"}>
-                                    <Image
-                                        alt={c.name}
-                                        className={"rounded-full object-cover"}
-                                        height={72.7}
-                                        src={c.avatar}
-                                        width={72.7}
-                                    />
-                                </div>
+                        <CardHeader className={"flex flex-col items-start text-left py-2"}>
+                            <div className={"flex gap-2 "}>
+                                <Image
+                                    alt={`Avatar của ${c.name}`}
+                                    className={"w-[60px] h-[60px] rounded-full overflow-hidden border border-black shadow-sm"}
+                                    height={60}
+                                    priority={false}
+                                    quality={85}
+                                    src={c.avatar}
+                                    width={60}
+                                />
+
                                 <div>
-                                    <CardTitle className={"text-lg font-semibold text-primary"}>
+                                    <CardTitle className={"font-bold text-lg text-primary"}>
                                         {c.name}
                                     </CardTitle>
-                                    <p className={"text-sm text-accent-foreground"}>{c.info}</p>
+                                    <p className={"text-sm text-accent-foreground"}>
+                                        {c.info}
+                                    </p>
                                 </div>
                             </div>
                         </CardHeader>
                         <CardContent>
-                            <p className={"text-sm text-accent-foreground leading-relaxed text-center"}>
-                                {c.description}
+                            <p className={"text-base text-accent-foreground leading-relaxed text-left"}>
+                                <span aria-hidden={"true"} className={"font-bold text-[#F25C5C]"}>
+                                    "
+                                </span>
+                                <span>{c.description}</span>
+                                <span aria-hidden={"true"} className={"font-bold text-[#F25C5C]"}>
+                                    "
+                                </span>
                             </p>
                         </CardContent>
                     </Card>
