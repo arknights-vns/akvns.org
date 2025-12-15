35bd860a63e3742f5e7b0652c4ab1a6aa72f59d9
Tien Dat Pham <dat20036@gmail.com>
2025-12-15 15:48:09 +0700

    finalize comic metadata schema.

--- src/schema/comic.ts
+++ src/schema/comic.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 
+import { ComicCategory } from "@/generated/prisma/enums";
 import { GalleryCollectionNameRegex } from "@/schema/gallery";
 
 /**
- * Represent a comic translator section.
+ * Represent a comic contributor.
  */
-export const ComicTranslator = z.object({
+export const ComicContributor = z.object({
     members: z.array(z.string()),
     role: z.string(),
 });
@@ -15,19 +16,32 @@ export const ComicTranslator = z.object({
  */
 export const ComicChapter = z.object({
     chapterName: z.string(),
+    comicSeriesId: z.string(),
     comicChapterId: z.string().regex(GalleryCollectionNameRegex),
 });
 
 /**
  * Minimal series metadata stored in DB.
+ *
+ * Use when querying a list of titles.
  */
 export const ComicSeriesMetadata = z.object({
-    author: z.string().min(1, { message: "Author is required" }),
-    category: z.string(),
-    comicChapters: z.array(ComicChapter).default([]),
+    category: z.enum(ComicCategory),
     comicSeriesId: z.string(),
-    synopsis: z.string(),
+    likeCount: z.int(),
     thumbnail: z.string(),
     title: z.string(),
-    translators: z.array(ComicTranslator).default([]),
+    updatedAt: z.iso.datetime(),
+    viewCount: z.int(),
+});
+
+/**
+ * Complete title data.
+ */
+export const ComicSeriesData = ComicSeriesMetadata.extend({
+    author: z.string().min(1, { message: "Author is required" }),
+    chapters: z.array(ComicChapter).default([]),
+    createdAt: z.iso.datetime(),
+    contributors: z.array(ComicContributor).default([]),
+    synopsis: z.string(),
 });
