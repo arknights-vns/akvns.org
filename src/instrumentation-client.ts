5d610afc52dbc92c517dd241726a5c775edf848f
Tien Dat Pham <dat20036@gmail.com>
2025-12-22 08:59:37 +0700

    move `.lintstagedrc.js` to `lint-staged.config.js` so Linux won't hide it again.
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -5,11 +5,7 @@ import { env } from "@/lib/env";
 posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: "/posthog",
     defaults: "2025-11-30",
-    fetch_options: {
-        cache: "force-cache",
-        next_options: {
-            revalidate: 60,
-            tags: ["posthog"],
-        },
-    },
+
+    autocapture: false,
+    capture_pageview: false,
 });
