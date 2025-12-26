10683dce08b12556b60e788cfc3da25c40db56e3
Tien Dat Pham <dat20036@gmail.com>
2025-12-26 20:34:12 +0700

    replace `ioredis` with one from Bun.
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,5 @@
-import { Redis } from "ioredis";
+import { RedisClient } from "bun";
 
-export const redis = new Redis(
+export const redis = new RedisClient(
     process.env.REDIS_URL || "redis://ligma:ligma@localhost:6379",
 );
