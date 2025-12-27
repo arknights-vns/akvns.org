45af0524a1674eef266b2b795a4ef8afc6da7676
Tien Dat Pham <dat20036@gmail.com>
2025-12-27 10:43:13 +0700

    retire database caching, since the API outlives it.
    let's hope the browser won't middle-finger us.
    
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,19 +1,9 @@
-import { RedisDrizzleCache } from "@databuddy/cache";
 import { SQL } from "bun";
 import { drizzle } from "drizzle-orm/bun-sql";
 
 import * as schema from "@/db/schema";
-import { redis } from "@/lib/redis";
-
-const cache = new RedisDrizzleCache({
-    redis,
-    defaultTtl: 600,
-    strategy: "all",
-    namespace: "akvns:cache",
-});
 
 export const drizzleDb = drizzle({
     client: new SQL(process.env.DATABASE_URL as string),
     schema,
-    cache,
 });
