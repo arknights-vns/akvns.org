33eec898e18d157e8b2228df64c9a8a86a1b1946
Tien Dat Pham <dat20036@gmail.com>
2025-12-28 12:58:36 +0700

    use `baseUrl` instead of hardcoding `akvns.org`
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,30 @@
 import type { MetadataRoute } from "next";
 
+const baseUrl =
+    process.env.NEXT_PUBLIC_PRODUCTION_URL || "http://localhost:3000";
+
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://akvns.org",
+            url: baseUrl,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 1,
         },
         {
-            url: "https://acme.com/staff",
+            url: `${baseUrl}/staff`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/blog`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.6,
+        },
+        {
+            url: `${baseUrl}/comic`,
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 0.8,
