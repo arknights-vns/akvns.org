44b7d80bd8c9c07eec3327a2ba838e41feb8200f
Tien Dat Pham <dat20036@gmail.com>
2025-12-28 18:34:34 +0700

    Elysia & Bun, (probably) ready for production!
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,27 +1,10 @@
-import openapi, { fromTypes } from "@elysiajs/openapi";
-import serverTiming from "@elysiajs/server-timing";
 import { Elysia } from "elysia";
-import z from "zod";
 
 import comicPlugin from "@/app/api/[[...slugs]]/blog/plugin";
 import blogPlugin from "@/app/api/[[...slugs]]/comic/plugin";
 import { auth } from "@/lib/auth";
 
 const app = new Elysia({ prefix: "/api" })
-    .use(
-        openapi({
-            references: fromTypes(),
-            mapJsonSchema: {
-                zod: z.toJSONSchema,
-            },
-            enabled: process.env.NODE_ENV === "development",
-        }),
-    )
-    .use(
-        serverTiming({
-            enabled: process.env.NODE_ENV === "development",
-        }),
-    )
     .use(blogPlugin)
     .use(comicPlugin)
     .mount(auth.handler);
