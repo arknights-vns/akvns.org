bfa4d683e2d2d32a2450bd3882f016563222c9cb
Tien Dat Pham <dat20036@gmail.com>
2025-12-29 19:43:56 +0700

    right.
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/lib/aws-s3.ts
+++ src/lib/aws-s3.ts
@@ -1,20 +1,12 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION || process.env.S3_AWS_REGION || "us-east-1",
+    region: process.env.S3_AWS_REGION || "us-east-1",
     endpoint:
-        process.env.AWS_ENDPOINT ||
-        process.env.S3_AWS_ENDPOINT ||
-        "https://s3.us-east-1.amazonaws.com",
+        process.env.S3_AWS_ENDPOINT || "https://s3.us-east-1.amazonaws.com",
     credentials: {
-        accessKeyId:
-            process.env.AWS_ACCESS_KEY_ID ||
-            process.env.S3_AWS_ACCESS_KEY_ID ||
-            "ligma",
-        secretAccessKey:
-            process.env.AWS_SECRET_ACCESS_KEY ||
-            process.env.S3_AWS_SECRET_ACCESS_KEY ||
-            "ligma",
+        accessKeyId: process.env.S3_AWS_ACCESS_KEY_ID || "ligma",
+        secretAccessKey: process.env.S3_AWS_SECRET_ACCESS_KEY || "ligma",
     },
 });
 
