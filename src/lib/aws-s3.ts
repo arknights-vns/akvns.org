f9e0f9d29c0b25788a01b44165bac188210bb721
Tien Dat Pham <dat20036@gmail.com>
2025-12-29 10:03:15 +0700

    did I say something about backward compatibility?
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/lib/aws-s3.ts
+++ src/lib/aws-s3.ts
@@ -1,10 +1,19 @@
 import { S3Client } from "bun";
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION || "https://s3.us-east-1.amazonaws.com",
-    endpoint: process.env.AWS_ENDPOINT || "us-east-1",
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "ligma",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "ligma",
+    region: process.env.AWS_REGION || process.env.S3_AWS_REGION || "us-east-1",
+    endpoint:
+        process.env.AWS_ENDPOINT ||
+        process.env.S3_AWS_ENDPOINT ||
+        "https://s3.us-east-1.amazonaws.com",
+    accessKeyId:
+        process.env.AWS_ACCESS_KEY_ID ||
+        process.env.S3_AWS_ACCESS_KEY_ID ||
+        "ligma",
+    secretAccessKey:
+        process.env.AWS_SECRET_ACCESS_KEY ||
+        process.env.S3_AWS_SECRET_ACCESS_KEY ||
+        "ligma",
 });
 
 export { s3Client };
