d18a345d9f34733ccb8ed46723dad6e4d36190f9
Tien Dat Pham <dat20036@gmail.com>
2026-01-18 22:15:19 +0700

    expand partner carousel to 99% of viewport.

--- src/components/landing-page/PartnersShowcase.tsx
+++ src/components/landing-page/PartnersShowcase.tsx
@@ -13,13 +13,12 @@ import { cn } from "@/lib/utils";
 
 export default function PartnersShowcase() {
   return (
-    <ContentArea className="container space-y-8 text-center" id="partners">
+    <ContentArea className="w-[99vw] space-y-8 text-center" id="partners">
       <Heading className="text-primary" kind="h1">
         Các đối tác của Arknights VNS
       </Heading>
       <Heading kind="h2">Fanpage</Heading>
       <Carousel
-        className="w-full"
         opts={{
           align: "center",
           loop: true,
@@ -30,21 +29,19 @@ export default function PartnersShowcase() {
           }),
         ]}
       >
-        <CarouselContent className="place-items-center-safe w-sm md:w-full">
+        <CarouselContent className="place-items-center-safe">
           {partnerList
             .filter((data) => data.type === "fanpage")
             .map((data) => (
               <CarouselItem
-                className="place-items-center-safe flex basis-1/2 flex-col justify-between gap-2 md:basis-1/4 lg:basis-1/5"
+                className="place-items-center-safe flex basis-1/2 flex-col gap-2 md:basis-1/4 lg:basis-1/5"
                 key={data.title}
               >
                 <Link className={cn(data.url === "#" && "pointer-events-none")} href={data.url as Route}>
                   <Image
                     alt={data.title}
-                    className="size-37.5 rounded-full border-2"
-                    fetchPriority="high"
+                    className="aspect-square size-37.5 rounded-full border-2"
                     height={150}
-                    referrerPolicy="no-referrer"
                     src={data.image}
                     width={150}
                   />
@@ -61,17 +58,15 @@ export default function PartnersShowcase() {
           .map((entry) => {
             return (
               <div className="place-items-center-safe flex flex-col gap-2" key={entry.title}>
-                <a className={cn(entry.url === "#" && "pointer-events-none")} href={entry.url}>
+                <Link className={cn(entry.url === "#" && "pointer-events-none")} href={entry.url as Route}>
                   <Image
                     alt={entry.title}
                     className="size-37.5 rounded-full border-2"
-                    fetchPriority="high"
                     height={150}
-                    referrerPolicy="no-referrer"
                     src={entry.image}
                     width={150}
                   />
-                </a>
+                </Link>
                 <FootNote className="font-bold text-lg">{entry.title}</FootNote>
               </div>
             );
