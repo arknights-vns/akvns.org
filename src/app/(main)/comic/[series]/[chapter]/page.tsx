37e1eab0843ba5add092afc33aa4b9f248828c97
Tien Dat Pham <dat20036@gmail.com>
2026-01-19 22:41:54 +0700

    /shrug

--- src/app/(main)/comic/[series]/[chapter]/page.tsx
+++ src/app/(main)/comic/[series]/[chapter]/page.tsx
@@ -7,11 +7,6 @@ import Link from "next/link";
 import { use, useEffect, useState } from "react";
 import { InView } from "react-intersection-observer";
 
-import {
-  ScrollProgress,
-  ScrollProgressContainer,
-  ScrollProgressProvider,
-} from "@/components/animate-ui/primitives/animate/scroll-progress";
 import { Button } from "@/components/ui/button";
 import { ButtonGroup } from "@/components/ui/button-group";
 import {
@@ -22,16 +17,20 @@ import {
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { ScrollProgress } from "@/components/ui/scroll-progress";
 import { comicImageQueryOptions, comicSeriesDataQueryOptions } from "@/query/comic";
 
 export default function ComicReadPage(props: PageProps<"/comic/[series]/[chapter]">) {
   const { series, chapter } = use(props.params);
   const [currentPage, setCurrentPage] = useState(1);
 
-
   useEffect(() => {
     localStorage.setItem(`comic-${series}`, chapter);
   }, [chapter, series]);
+
+  useEffect(() => {
+    localStorage.setItem(`comic-${series}-page`, currentPage.toString());
+  }, [series, currentPage]);
 
   const { data: seriesData } = useSuspenseQuery(comicSeriesDataQueryOptions(series));
   const { data: serverImages } = useSuspenseQuery(comicImageQueryOptions({ series, chapter }));
@@ -59,91 +58,90 @@ export default function ComicReadPage(props: PageProps<"/comic/[series]/[chapter
   });
 
   return (
-    <ScrollProgressProvider global={true}>
-      <div className="flex flex-col items-center gap-4">
-        <aside className="place-items-center-safe sticky top-0 z-2 flex w-full flex-col gap-2 bg-background/75 pt-1 backdrop-blur-lg">
-          <div className="flex gap-1 text-lg">
-            <Link className="hover:underline" href={`/comic/${series}`}>
-              {seriesData.title}
-            </Link>
-          </div>
-          <div className="place-items-center-safe flex justify-between gap-2">
-            <Button disabled={!hasPrev}>
-              <Link
-                className="place-items-center-safe flex gap-1"
-                href={hasPrev ? `/comic/${series}/${listOfChapters[currentPosition - 1]}` : "/"}
-              >
-                <ArrowLeft />
-                <span className="hidden md:inline">Chương trước</span>
-              </Link>
-            </Button>
-
-            <div className="place-items-center-safe gap-4">
-              <ButtonGroup>
-                {/* Chapter select */}
-                <DropdownMenu>
-                  <DropdownMenuTrigger
-                    render={
-                      <Button>
-                        <BookOpen />
-                        {chapterName}
-                      </Button>
-                    }
-                  />
-                  <DropdownMenuContent align="start" className="w-56">
-                    <DropdownMenuRadioGroup value={chapter}>
-                      {seriesData.chapters.map((entry) => (
-                        <DropdownMenuRadioItem key={entry.id} value={entry.id}>
-                          <Link href={`/comic/${series}/${entry.comicChapterId}`}>{entry.chapterName}</Link>
-                        </DropdownMenuRadioItem>
-                      ))}
-                    </DropdownMenuRadioGroup>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-                {/* Jump to page */}
-                <DropdownMenu>
-                  <DropdownMenuTrigger
-                    render={
-                      <Button>
-                        <StickyNote />
-                        Nhảy đến
-                      </Button>
-                    }
-                  />
-                  <DropdownMenuContent align="start" className="w-56">
-                    {serverImages.map((entry, index) => (
-                      <DropdownMenuItem key={entry.url}>
-                        <Link href={{ href: `/comic/${series}/${chapter}`, hash: `page-${index + 1}` }}>
-                          Trang {index + 1}
-                        </Link>
-                      </DropdownMenuItem>
-                    ))}
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </ButtonGroup>
-            </div>
-            <Button disabled={!hasNext}>
-              <Link
-                className="place-items-center-safe flex gap-1"
-                href={hasNext ? `/comic/${series}/${listOfChapters[currentPosition + 1]}` : "/"}
-              >
-                <span className="hidden md:inline">Chương kế</span>
-                <ArrowRight />
-              </Link>
-            </Button>
-          </div>
-          <ScrollProgress className="h-1 self-start rounded-r-full rounded-l-full bg-primary" />
-        </aside>
-        <ScrollProgressContainer className="flex flex-col gap-2">
-          {images.map((x) => x)}
-        </ScrollProgressContainer>
-        <Button className="my-8 p-4">
-          <Link href={{ href: `/comic/${series}/${chapter}`, hash: "page-1" }}>
-            <ArrowUpFromLine />
-            Về trang nhất
+    <div className="flex flex-col items-center gap-4">
+      <aside className="place-items-center-safe sticky top-0 z-2 flex w-full flex-col gap-2 bg-background/75 p-2 pt-1 backdrop-blur-lg">
+        <div className="flex gap-1 text-lg">
+          <Link className="hover:underline" href={`/comic/${series}`}>
+            {seriesData.title}
           </Link>
-        </Button>
-      </div>
-    </ScrollProgressProvider>
+        </div>
+        <div className="place-items-center-safe flex justify-between gap-2">
+          <Button disabled={!hasPrev}>
+            <Link
+              className="place-items-center-safe flex gap-1"
+              href={hasPrev ? `/comic/${series}/${listOfChapters[currentPosition - 1]}` : "/"}
+            >
+              <ArrowLeft />
+              <span className="hidden md:inline">Chương trước</span>
+            </Link>
+          </Button>
+
+          <div className="place-items-center-safe gap-4">
+            <ButtonGroup>
+              {/* Chapter select */}
+              <DropdownMenu>
+                <DropdownMenuTrigger
+                  render={
+                    <Button>
+                      <BookOpen />
+                      {chapterName}
+                    </Button>
+                  }
+                />
+                <DropdownMenuContent align="start" className="w-56">
+                  <DropdownMenuRadioGroup value={chapter}>
+                    {seriesData.chapters.map((entry) => (
+                      <DropdownMenuRadioItem key={entry.id} value={entry.id}>
+                        <Link href={`/comic/${series}/${entry.comicChapterId}`}>{entry.chapterName}</Link>
+                      </DropdownMenuRadioItem>
+                    ))}
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              {/* Jump to page */}
+              <DropdownMenu>
+                <DropdownMenuTrigger
+                  render={
+                    <Button>
+                      <StickyNote />
+                      Nhảy đến
+                    </Button>
+                  }
+                />
+                <DropdownMenuContent align="start" className="w-56">
+                  {serverImages.map((entry, index) => (
+                    <DropdownMenuItem key={entry.url}>
+                      <Link href={{ href: `/comic/${series}/${chapter}`, hash: `page-${index + 1}` }}>
+                        Trang {index + 1}
+                      </Link>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </ButtonGroup>
+          </div>
+          <Button disabled={!hasNext}>
+            <Link
+              className="place-items-center-safe flex gap-1"
+              href={hasNext ? `/comic/${series}/${listOfChapters[currentPosition + 1]}` : "/"}
+            >
+              <span className="hidden md:inline">Chương kế</span>
+              <ArrowRight />
+            </Link>
+          </Button>
+        </div>
+        <ScrollProgress className="h-1 self-start rounded-r-full rounded-l-full bg-primary" />
+      </aside>
+      <div className="flex flex-col gap-2">{images.map((x) => x)}</div>
+      <Button className="my-8 p-4">
+        <Link
+          className="place-items-center-safe flex gap-2"
+          href={{ pathname: `/comic/${series}/${chapter}`, hash: "page-1" }}
+        >
+          <ArrowUpFromLine />
+          Về trang nhất
+        </Link>
+      </Button>
+    </div>
   );
 }
