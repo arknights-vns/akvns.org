a1cbf25153c54afaad8317c84d05a5c3ad9d0af3
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-01-23 11:03:27 +0700

    initialize Dreamchasers/Offline '25 project page

---
+++ src/app/(main)/projects/dreamchasers/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ContentArea from "@/components/ContentArea";
+
+export default function offline25() {
+  return (
+    <ContentArea>
+      <p>VNS Offline 2025 - Dreamchasers</p>
+      <p>content content content</p>
+    </ContentArea>
+  );
+}
