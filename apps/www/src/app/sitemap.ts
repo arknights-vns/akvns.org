db2120c8f15a9338bf14027f6e9b482fbf2e4d96
Tien Dat Pham <dat20036@gmail.com>
2026-02-19 12:30:50 +0700

    throw all possble comic pages on sitemap.

--- apps/www/src/app/sitemap.ts
+++ apps/www/src/app/sitemap.ts
@@ -1,43 +1,68 @@
 import type { MetadataRoute } from "next";
 import { comicSeries } from "@/db/schema/vns-schema";
 import { clientEnv } from "@/env-var/client";
+import { fetchComicSeriesData } from "@/functions/comic/fetch-series-data";
 import { drizzleDb } from "@/lib/drizzle";
 
 const PAGE_URL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : clientEnv.NEXT_PUBLIC_PRODUCTION_URL;
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // Google's limit is 50,000 URLs per sitemap
-  // minus first 3 entries.
-  const entries = await drizzleDb
-    .select({ seriesId: comicSeries.comicSeriesId })
-    .from(comicSeries)
-    .limit(50_000 - 3);
+const trailingSlashRegex = /\/$/;
+const cleanedUrl = PAGE_URL.endsWith("/") ? PAGE_URL.replace(trailingSlashRegex, "") : PAGE_URL;
 
-  return [
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const entries = await drizzleDb.select({ seriesId: comicSeries.comicSeriesId }).from(comicSeries);
+
+  const pages: MetadataRoute.Sitemap = [
     {
-      url: PAGE_URL,
+      url: cleanedUrl,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: `${PAGE_URL}/staff`,
+      url: `${cleanedUrl}/staff`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: `${PAGE_URL}/comic`,
+      url: `${cleanedUrl}/comic`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
-    ...entries.map((entry) => ({
-      url: `${PAGE_URL}/${entry.seriesId}`,
+    {
+      url: `${cleanedUrl}/projects`,
       lastModified: new Date(),
-      changeFrequency: "weekly" as const,
+      changeFrequency: "daily",
       priority: 0.8,
-    })),
+    },
   ];
+
+  for (const entry of entries) {
+    const seriesData = await fetchComicSeriesData(entry.seriesId);
+
+    pages.push({
+      url: `${cleanedUrl}/${seriesData?.comicSeriesId}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    });
+
+    if (!seriesData) {
+      continue;
+    }
+
+    for (const chapter of seriesData.chapters) {
+      pages.push({
+        url: `${cleanedUrl}/${seriesData?.comicSeriesId}/${chapter.comicChapterId}`,
+        lastModified: new Date(),
+        changeFrequency: "never",
+        priority: 0.5,
+      });
+    }
+  }
+
+  return pages;
 }
