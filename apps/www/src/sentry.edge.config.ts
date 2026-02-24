4a287237296d4dc8d51f34cab45648bde1893de7
Tien Dat Pham <dat20036@gmail.com>
2026-02-24 10:31:33 +0700

    make Sentry STFU in `development` environment.

--- apps/www/src/sentry.edge.config.ts
+++ apps/www/src/sentry.edge.config.ts
@@ -1,8 +1,11 @@
 import { init } from "@sentry/nextjs";
 
-init({
-  dsn: "https://23fb1ca2739569b0ed844ac20c498825@o4510706038734848.ingest.de.sentry.io/4510706043912272",
-  tracesSampleRate: 1,
-  enableLogs: true,
-  sendDefaultPii: true,
-});
+// https://docs.sentry.io/platforms/javascript/configuration/options/#enabled
+// "To disable Sentry completely, depending on environment, call Sentry.init conditionally."
+process.env.NODE_ENV === "production" &&
+  init({
+    dsn: "https://23fb1ca2739569b0ed844ac20c498825@o4510706038734848.ingest.de.sentry.io/4510706043912272",
+    tracesSampleRate: 1,
+    enableLogs: true,
+    sendDefaultPii: true,
+  });
