a377e0dc57861c7dfc66811d48604a3c2c971d42
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-02-27 22:49:05 +0700

    add partial remaining comic data

--- packages/database/scripts/seed.ts
+++ packages/database/scripts/seed.ts
@@ -48,6 +48,46 @@ async function seed() {
           synopsis:
             "Một câu chuyện giữa Franka, Liskarm, Vanilla, và Jessica từ Blacksteel Worldwide trước khi họ trở thành Cán Viên của Rhodes Island",
         },
+        {
+          series_id: "sui-daily-slices-mundane-mortal-life",
+          title: "Tuế Gia Thường Nhật : Kiếp Sống Nhân Gian",
+          thumbnail: "https://comic-assets.akvns.org/sui-daily-slices-mundane-mortal-life/chapter-0/00.jpg",
+          author: "Terra Historicus",
+          category: "Arknights_VNS",
+          synopsis: "n/a",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          title: "Rhodes Kitchen: Tidbits",
+          thumbnail: "",
+          author: "Hypergryph",
+          category: "Arknights_VNS",
+          synopsis: "fixme",
+        },
+        {
+          series_id: "octodeco",
+          title: "Octodeco",
+          thumbnail: "https://comic-assets.akvns.org/octodeco/dont-be-lonely-alone/00.jpg",
+          author: "Octodeco",
+          category: "Arknights_VNS",
+          synopsis: "fixme",
+        },
+        {
+          series_id: "logos-and-rosmontis",
+          title: "Logos & Rosmontis",
+          thumbnail: "",
+          author: "Terra Historicus",
+          category: "Arknights_VNS",
+          synopsis: "fixme",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          title: "Records of Originium: Rhine Lab",
+          thumbnail: "",
+          author: "Terra Historicus",
+          category: "Arknights_VNS",
+          synopsis: "fixme",
+        },
       ],
     });
 
@@ -104,8 +144,182 @@ async function seed() {
           chapter_id: "special-3",
           chapter_name: "Chương Đặc Biệt 3: Đeo vào đi",
         },
+        {
+          series_id: "sui-daily-slices-mundane-mortal-life",
+          chapter_id: "chapter-0",
+          chapter_name: "One-Shot",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-1",
+          chapter_name: "Chapter 1",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-2",
+          chapter_name: "Chapter 2",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-3",
+          chapter_name: "Chapter 3",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-4",
+          chapter_name: "Chapter 4",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-5",
+          chapter_name: "Chapter 5",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-6",
+          chapter_name: "Chapter 6",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-7",
+          chapter_name: "Chapter 7",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-8",
+          chapter_name: "Chapter 8",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-9",
+          chapter_name: "Chapter 9",
+        },
+        {
+          series_id: "rhodes-kitchen-tidbits",
+          chapter_id: "chapter-10",
+          chapter_name: "Chapter 10",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "dont-be-lonely-alone",
+          chapter_name: "Don't be Lonely, Alone",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "monhun-1",
+          chapter_name: "Monster Hunter Reaction 1",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "monhun-2",
+          chapter_name: "Monster Hunter Reaction 2",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "monhun-3",
+          chapter_name: "Monster Hunter Reaction 3",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "side-33",
+          chapter_name: "Side Story 33",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "side-34",
+          chapter_name: "Side Story 34",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "side-35",
+          chapter_name: "Side Story 35",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "side-40",
+          chapter_name: "Side Story 40",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "side-58",
+          chapter_name: "Side Story 58",
+        },
+        {
+          series_id: "octodeco",
+          chapter_id: "side-60",
+          chapter_name: "Side Story 60",
+        },
+        {
+          series_id: "logos-and-rosmontis",
+          chapter_id: "chapter-0",
+          chapter_name: "One-Shot",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-0",
+          chapter_name: "Chapter 0",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-1",
+          chapter_name: "Chapter 1",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-2",
+          chapter_name: "Chapter 2",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-3",
+          chapter_name: "Chapter 3",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-4",
+          chapter_name: "Chapter 4",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-5",
+          chapter_name: "Chapter 5",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-6",
+          chapter_name: "Chapter 6",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-7",
+          chapter_name: "Chapter 7",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-8",
+          chapter_name: "Chapter 8",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "chapter-9",
+          chapter_name: "Chapter 9",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "special-1",
+          chapter_name: "Special Chapter 1",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "special-2",
+          chapter_name: "Special Chapter 2",
+        },
+        {
+          series_id: "records-of-originium-rhine-lab",
+          chapter_id: "special-3",
+          chapter_name: "Special Chapter 3",
+        },
       ],
-      /// ================================]
     });
   });
 }
