e44ecb15acf7631f0effd3e87002ac4bd4c85359
Tien Dat Pham <dat20036@gmail.com>
2026-03-02 10:30:33 +0700

    use LoadingLappy for fallback UI of `/comic`
    for subpaths of `/comic`, probably less likely to be seen.

--- apps/www/src/app/(main)/comic/layout.tsx
+++ apps/www/src/app/(main)/comic/layout.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
+import LoadingLappy from "@/components/LoadingLappy";
 
 export default function ComicPageLayout(props: LayoutProps<"/comic">) {
   // yes, we ran in full circle.
-  return <Suspense>{props.children}</Suspense>;
+  return <Suspense fallback={<LoadingLappy />}>{props.children}</Suspense>;
 }
