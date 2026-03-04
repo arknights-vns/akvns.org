e95f67c3e8b73d20c4ddd80d39a728903915805e
Tien Dat Pham <dat20036@gmail.com>
2026-03-04 10:28:02 +0700

    add missing <html> & <body> tag.

--- apps/www/src/app/global-error.tsx
+++ apps/www/src/app/global-error.tsx
@@ -6,7 +6,7 @@ import { captureException } from "@sentry/nextjs";
 import Image from "next/image";
 import { useEffect } from "react";
 
-export default function SiteError({
+export default function GlobalError({
   error,
   // biome-ignore lint/correctness/noUnusedFunctionParameters: required by Next.js?
   reset,
@@ -19,18 +19,22 @@ export default function SiteError({
   }, [error]);
 
   return (
-    <div className="place-items-center-safe flex h-[90lvh] flex-col justify-evenly">
-      <div />
-      <div className="place-items-center-safe flex max-w-[90vw] flex-col gap-4">
-        <Image alt="error image" className="max-w-[90vw]" src={We_Are_Sorry} width={520} />
-        <Heading className="text-center text-primary" kind="h2">
-          Well... đã có lỗi xảy ra, và tụi mình rât tiếc về điều đó.
-        </Heading>
-        <FavorText className="text-center">
-          Lỗi này đã được ghi nhận, tụi mình sẽ <span className="italic">cố gắng</span> khắc phục trong thời
-          gian sớm nhất!
-        </FavorText>
-      </div>
-    </div>
+    <html lang="vi">
+      <body>
+        <div className="place-items-center-safe flex h-[90lvh] flex-col justify-evenly">
+          <div />
+          <div className="place-items-center-safe flex max-w-[90vw] flex-col gap-4">
+            <Image alt="error image" className="max-w-[90vw]" src={We_Are_Sorry} width={520} />
+            <Heading className="text-center text-primary" kind="h2">
+              Well... đã có lỗi xảy ra, và tụi mình rất tiếc về điều đó.
+            </Heading>
+            <FavorText className="text-center">
+              Lỗi này đã được ghi nhận, tụi mình sẽ <span className="italic">cố gắng</span> khắc phục trong
+              thời gian sớm nhất!
+            </FavorText>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
