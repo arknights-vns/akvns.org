060ca138c827802dbcbdd8d18719481792cde920
Tien Dat Pham <dat20036@gmail.com>
2026-03-09 21:35:03 +0700

    don't ship TanStack Query onto prod, for now.

--- apps/www/src/components/Providers.tsx
+++ apps/www/src/components/Providers.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
-import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <ReactQueryProvider>{children}</ReactQueryProvider>;
+  // return <ReactQueryProvider>{children}</ReactQueryProvider>;
+  return children;
 }
