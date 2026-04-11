59f7526308484403940ee4c70301c27f5ff5c725
Tien Dat Pham <dat20036@gmail.com>
2026-04-11 10:19:01 +0700

    combine husky + lint-staged to lefthook.

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-// noinspection JSUnusedGlobalSymbols
-
-/**
- * @filename: lint-staged.config.js
- * @type {import('lint-staged').Configuration}
- */
-export default {
-  "*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}": ["oxlint", "oxfmt"],
-};
